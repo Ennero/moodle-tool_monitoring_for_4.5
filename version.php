54a48ef93c2eafa460369903ad0c1a06003cfb6f
ennero <2895884051401@ingenieria.usac.edu.gt>
2026-09-22 09:00:17 -0600

    build: target Moodle 4.5 only

--- version.php
+++ version.php
@@ -34,6 +34,6 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'tool_monitoring';
 $plugin->release   = '1.1.0';
 $plugin->version   = 2026073100;
-$plugin->requires  = 2025041400; // Moodle 5.0.
-$plugin->supported = [500, 502];
+$plugin->requires  = 2024100700; // Moodle 4.5.
+$plugin->supported = [405, 405];
 $plugin->maturity  = MATURITY_STABLE;
